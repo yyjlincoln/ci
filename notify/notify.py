6101435cfa1fd25dd67e9d759c1108ae42cb2e87
Lincoln Yan <yyjlincoln@gmail.com>
2022-03-15 16:00:12 +1100

    [feat] Added notify

---
+++ notify/notify.py
@@ -0,0 +1,64 @@
+import requests
+import threading
+import json
+import time
+import argparse
+
+
+def _send_discord_msg(messageType, *args, **kw):
+    discord_colors = {
+        'log': 0x000000,
+        'debug': 0x000000,
+        'info': 0x00FF00,
+        'error': 0xFF0000,
+        'fatal': 0xFF0000,
+        'warning': 0xFFFF00
+    }
+    HOOK = DISCORD_WEBHOOK
+    if not HOOK:
+        return
+
+    requests.post(HOOK,
+                  data=json.dumps({
+                      'content': '',
+                      'username': 'Github-CI',
+                      'embeds': [{
+                          'title': serviceName+": "+messageType,
+                          'description': 'At ' +
+                          time.strftime('%Y/%m/%d %H:%M:%S', time.localtime()),
+                          'fields': [{
+                                  'name': 'Message',
+                                  'value': ' '.join(list(map(lambda x: str(x),
+                                                             args)))
+                          }],
+                          'color': discord_colors.get(messageType, 0x000000)
+                      }]
+                  }), headers={'Content-Type': 'application/json'})
+
+
+def send_discord_msg(messageType, *args, **kw):
+    threading.Thread(target=_send_discord_msg,
+                     args=(messageType, *args), kwargs=kw).start()
+
+
+with open('secrets.json') as f:
+    SECRETS = json.load(f)
+
+DISCORD_WEBHOOK = SECRETS['discord_webhook']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--type', '-t', help='Type', default='log',
+                    choices=['log', 'debug', 'info',
+                             'error', 'fatal', 'warning'],
+                    nargs=1
+                    )
+parser.add_argument('--message', '-m', help='Message', required=True, nargs=1)
+parser.add_argument('--name', '-n', help='Service Name',
+                    default=["Unknown Service"], nargs=1)
+
+args = parser.parse_args()
+
+msgType = args.type[0]
+msg = args.message[0]
+serviceName = args.name[0]
+send_discord_msg(msgType, msg)
